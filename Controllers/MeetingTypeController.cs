769d82e9277dba8857733d1f41498670c24db650
Babar-Meet <babarmeet677@gmail.com>
2025-12-30 19:23:20 +0530

    Initial commit

---
+++ Controllers/MeetingTypeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MOM_Project.Controllers
+{
+    public class MeetingTypeController : Controller
+    {
+        public IActionResult MeetingTypeList()
+        {
+            return View();
+        }
+
+        public IActionResult MeetingTypeAddEdit()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/MeetingsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MOM_Project.Controllers
+{
+    public class MeetingsController : Controller
+    {
+        public IActionResult MeetingList()
+        {
+            return View();
+        }
+        public IActionResult MeetingAddEdit()
+        {
+            return View();
+        }
+    }
+}
